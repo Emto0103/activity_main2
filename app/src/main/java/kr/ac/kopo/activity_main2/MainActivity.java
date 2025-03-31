c7bd4b05c293635da366206a6eff9a80fb0748ce
emto0103 <emto0103@gmail.com>
2025-03-31 16:03:48 +0900

    TextView 설정

--- app/src/main/java/kr/ac/kopo/activity_main2/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/activity_main2/MainActivity.java
@@ -2,12 +2,16 @@ package kr.ac.kopo.activity_main2;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +32,19 @@ public class MainActivity extends AppCompatActivity {
         text3.setTextColor(Color.MAGENTA);
         text3.setText("singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성singleline속성");
         text3.setSingleLine();
+
+        EditText edit1 = findViewById(R.id.edit1);
+        TextView textResult = findViewById(R.id.textResult);
+        Button btnResult = findViewById(R.id.btnResult);
+
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String univName = edit1.getText().toString();
+                String result = univName + "에 합격하신 것을 진심으로 축하드립니다.";
+                textResult.setText(result);
+                textResult.setTextColor(Color.rgb(128,65,217));
+            }
+        });
     }
 }
